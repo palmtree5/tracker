d8af66e9f077fb16fa53cc3ae16e85d51230ff26
root <vlasblom.p@outlook.com>
2016-04-30 16:58:44 +0200

    Automatic update

--- track.py
+++ track.py
@@ -2,6 +2,7 @@ import os
 import urllib2
 
 file = open("files/blockedservers", "w")
+
 for line in urllib2.urlopen("https://sessionserver.mojang.com/blockedservers"):
 	found = False
 	for dict_line in open("dictionary.txt", "r"):
@@ -11,7 +12,3 @@ for line in urllib2.urlopen("https://sessionserver.mojang.com/blockedservers"):
 
 	if not found:
 		file.write(line.strip() + "\n")
-
-os.system("git add .")
-os.system("git commit -a -m \"Automatic update\"")
-os.system("git push -u origin master")
