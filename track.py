d770580db6f0bc3ba92e2b0dfb7593e4c4dc07c1
root <vlasblom.p@outlook.com>
2016-05-01 00:22:50 +0200

    Manual update

--- track.py
+++ track.py
@@ -5,7 +5,7 @@ file = open("files/blockedservers", "w")
 
 for line in urllib2.urlopen("https://sessionserver.mojang.com/blockedservers"):
 	found = False
-	for dict_line in open("dictionary.txt", "r"):
+	for dict_line in open("files/dictionary.txt", "r"):
 		if line.strip() in dict_line.strip():
 			file.write(dict_line.strip() + "\n")
 			found = True
